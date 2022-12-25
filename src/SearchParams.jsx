bf1dfaa92a1f848f32bdf39ba59f9ff8c9029c90
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-25 12:47:05 +0200

    add useState for animals

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-
+const ANIMALS = ["bird", "cat", "dog", 'rabbit', "reptile"];
 const SearchParams = () => {
-    const [location, setLocation] = useState('Seattle, WA')
+    const [location, setLocation] = useState('')
+    const [animal, setAnimals] = useState('')
     return (
         <div className="search-params">
             <form>
@@ -11,6 +12,15 @@ const SearchParams = () => {
                         setLocation(e.target.value)
                     }} type="text" id="location" value={location}
                     placeholder='Location' />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select name="animal" id="animal"
+                    value={animal} onChange={(e) => {
+                        setAnimals(e.target.value)
+                    }}>
+                    {ANIMALS.map((animal) => <option key={animal}>{animal}</option> )}
+                    </select>
                 </label>
                 <button type="submit">Submit</button>
             </form>
