11cdfd4df7c93aba5b0802ae831cef6ee3b26fbc
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-25 17:49:42 +0200

    add fetch methode for reactQuery

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./details";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    staleTime: Infinity,
+    cacheTime: Infinity,
+  }
+})
 
 const App = () => {
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
      <header>
           <Link to="/">Adopt Me!</Link>
         </header>
@@ -13,6 +24,7 @@ const App = () => {
     <Route path="/details/:id" element={<Details />} />
     <Route path="/" element={<SearchParams />} />
   </Routes>
+  </QueryClientProvider>
     </BrowserRouter>
   );
 };
